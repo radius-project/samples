4b4ea85286fa99a08d3951dabde933bc9e4e403d
Reshma Abdul Rahim <61033581+Reshrahim@users.noreply.github.com>
2023-05-24 09:17:08 -0400

    Keep post creation only to cluster creation

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,17 +6,4 @@ k3d cluster delete
 
 k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:0'
 
-RAD_VERSION=$(rad version | awk 'NR==2{print $1}')
-
-if [ "$RAD_VERSION" = "edge" ]; then
-    wget -q "https://get.radapp.dev/tools/rad/install.sh" -O - | /bin/bash -s edge
-fi
-
-rad install kubernetes --set global.rp.publicEndpointOverride=localhost:8081
-rad group create default
-rad workspace create kubernetes default --group default
-rad group switch default
-rad env create default
-rad env switch default
-
 echo "Ending Post Create Command"
