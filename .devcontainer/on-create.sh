8113ecd551383b52a1055e8e72c88acc2170bc24
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-05-25 14:29:42 -0700

    Change order of Dapr init

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-## Install Dapr
-wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
-dapr uninstall # clean if needed
-dapr init -k
-
 ## Create a k3d cluster
 k3d cluster delete
 k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:0'
+
+## Install Dapr and init
+wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
+dapr uninstall # clean if needed
+dapr init -k
 
 ## Install rad CLI
 CURRENT_BRANCH=$(git branch --show-current)
