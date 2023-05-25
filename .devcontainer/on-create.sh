5235c76158ad9fbfe2c8f20ab7a65bb3201646a0
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-05-25 20:48:11 +0000

    Fix names

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+## Install Dapr
+wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
+dapr uninstall # clean if needed
+dapr init
+
+## Create a k3d cluster
+k3d cluster delete
+k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:0'
+
 ## Install rad CLI
 CURRENT_BRANCH=$(git branch --show-current)
 
@@ -16,10 +25,7 @@ else
 fi
 
 if [ "$RADIUS_VERSION" = "edge" ]; then
-    curl https://get.radapp.dev/tools/vscode-extensibility/edge/rad-vscode-bicep.vsix --output rad-vscode-bicep.vsix
+    wget -q "https://radiuspublic.blob.core.windows.net/tools/rad/install.sh" -O - | /bin/bash -s edge
 else
-    curl https://get.radapp.dev/tools/vscode-extensibility/stable/rad-vscode-bicep.vsix --output rad-vscode-bicep.vsix
+    wget -q "https://get.radapp.dev/tools/rad/install.sh" -O - | /bin/bash
 fi
-
-code --install-extension rad-vscode-bicep.vsix
-rm rad-vscode-bicep.vsix
