1ca66400e7ee3f51acac475001515260f293c15e
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-05-26 23:20:27 +0000

    Change Bicep installation step

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -9,26 +9,6 @@ wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O 
 dapr uninstall # clean if needed
 dapr init -k
 
-## Install rad CLI
-CURRENT_BRANCH=$(git branch --show-current)
+## Install stable rad CLI (edge conditionally downloaded in post-create script)
+wget -q "https://get.radapp.dev/tools/rad/install.sh" -O - | /bin/bash
 
-if [ "$CURRENT_BRANCH" = "edge" ]; then
-    RADIUS_VERSION=edge
-else
-    ## If CURRENT_BRANCH matches a regex of the form "v0.20", set RADIUS_VERSION to the matching string minus the "v"
-    if [[ "$CURRENT_BRANCH" =~ ^v[0-9]+\.[0-9]+$ ]]; then
-        RADIUS_VERSION=${CURRENT_BRANCH:1}
-    else
-        ## Otherwise, set RADIUS_VERSION to "edge"
-        RADIUS_VERSION=edge
-    fi
-fi
-
-if [ "$RADIUS_VERSION" = "edge" ]; then
-    wget -q "https://get.radapp.dev/tools/rad/install.sh" -O - | /bin/bash -s edge
-else
-    wget -q "https://get.radapp.dev/tools/rad/install.sh" -O - | /bin/bash
-fi
-
-## Download Bicep extension
-curl https://get.radapp.dev/tools/vscode-extensibility/$RADIUS_VERSION/rad-vscode-bicep.vsix --output /tmp/rad-vscode-bicep.vsix
