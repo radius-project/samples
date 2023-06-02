2e7acbf975cdd7d6ac622da2baa5755caba1b40a
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-06-02 13:28:45 -0700

    Add check for node creation

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -4,6 +4,13 @@
 k3d cluster delete
 k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:0'
 
+## Verify cluster was created and exit 1 if not
+kubectl get nodes
+if [ $? -ne 0 ]; then
+    echo "Failed to create k3d cluster"
+    exit 1
+fi
+
 ## Install Dapr and init
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
 dapr uninstall # clean if needed
@@ -11,4 +18,3 @@ dapr init -k
 
 ## Install stable rad CLI (edge conditionally downloaded in post-create script)
 wget -q "https://get.radapp.dev/tools/rad/install.sh" -O - | /bin/bash
-
