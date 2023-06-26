c398550d4c63376f09f34c5ab26532c5b2b045b8
jasonviviano <83607984+jasonviviano@users.noreply.github.com>
2023-06-26 17:36:30 +0000

    Fixed Bicep extension installation

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+## Install branch-specific Bicep extension
+CURRENT_BRANCH=$(git branch --show-current)
+
+if [ "$CURRENT_BRANCH" = "edge" ]; then
+    RADIUS_VERSION=edge
+else
+    ## If CURRENT_BRANCH matches a regex of the form "v0.20", set RADIUS_VERSION to the matching string minus the "v"
+    if [[ "$CURRENT_BRANCH" =~ ^v[0-9]+\.[0-9]+$ ]]; then
+        RADIUS_VERSION=${CURRENT_BRANCH:1}
+    else
+        ## Otherwise, set RADIUS_VERSION to "edge"
+        RADIUS_VERSION=edge
+    fi
+fi
+
+## Download Bicep extension
+curl https://get.radapp.dev/tools/vscode-extensibility/$RADIUS_VERSION/rad-vscode-bicep.vsix --output /tmp/rad-vscode-bicep.vsix
