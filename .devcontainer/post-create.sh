514c7a333b9d0394ca4cd22fea9369b6f8c3526c
jasonviviano <83607984+jasonviviano@users.noreply.github.com>
2023-06-27 15:11:57 +0000

    Handle  race condition

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -18,5 +18,8 @@ fi
 ## Download Bicep extension
 curl https://get.radapp.dev/tools/vscode-extensibility/$RADIUS_VERSION/rad-vscode-bicep.vsix --output .devcontainer/rad-vscode-bicep.vsix
 
+## Race condition workaround for `code --install-extension` command
+at now + 1 minute
+
 ## Install Radius Bicep extension
 code --install-extension .devcontainer/rad-vscode-bicep.vsix
