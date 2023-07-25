3e2f3d9ccf32143f74ca85583bac3eb03a32396d
willdavsmith <willdavsmith@gmail.com>
2023-07-25 14:05:20 -0700

    Fixing semver

--- .github/scripts/validate_semver.py
+++ .github/scripts/validate_semver.py
@@ -26,9 +26,7 @@ def main():
         sys.exit(1)
 
     # From https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-    # Group 'version' returns the whole version
-    # other named groups return the components
-    SEMVER_REGEX = r"(?P<version>0|(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$"
+    SEMVER_REGEX = r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
 
     pattern = re.compile(SEMVER_REGEX)
 
