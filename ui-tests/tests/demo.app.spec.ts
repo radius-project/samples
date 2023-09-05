8c1ae1e1605918c52aa83c3e74cf7f1807b4cff3
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-09-05 16:44:50 -0700

    Update identifiers

--- ui-tests/tests/demo.app.spec.ts
+++ ui-tests/tests/demo.app.spec.ts
@@ -39,12 +39,9 @@ test("To-Do App Basic UI Checks", async ({ page }) => {
     .click();
 
   // Make sure important environment variables are visible
-  await expect(page.getByText("CONNECTION_REDIS_CONNECTIONSTRING"))
-    .toBeVisible();
-  await expect(page.getByText("CONNECTION_REDIS_HOST"))
-    .toBeVisible();
-  await expect(page.getByText("CONNECTION_REDIS_PORT"))
-    .toBeVisible();
+  await expect(page.getByRole('cell', { name: 'CONNECTION_REDIS_CONNECTIONSTRING' }).getByText('CONNECTION_REDIS_CONNECTIONSTRING')).toBeVisible();
+  await expect(page.getByRole('cell', { name: 'CONNECTION_REDIS_HOST' }).getByText('CONNECTION_REDIS_HOST')).toBeVisible();
+  await expect(page.getByRole('cell', { name: 'CONNECTION_REDIS_PORT' }).getByText('CONNECTION_REDIS_PORT')).toBeVisible();
 });
 
 test("Add an item and check basic functionality", async ({ page }) => {
