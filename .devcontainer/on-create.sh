346d432378a8b2a27664c2b961c089f494ae3885
Reshma Abdul Rahim <reshmarahim.abdul@microsoft.com>
2023-10-17 17:59:18 -0400

    Update vscode extension install

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -20,6 +20,3 @@ if [ "$RADIUS_VERSION" = "edge" ]; then
 else
     wget -q "https://raw.githubusercontent.com/radius-project/radius/main/deploy/install.sh" -O - | /bin/bash
 fi
-
-## Download Bicep extension
-curl https://github.com/radius-project/bicep/releases/download/$RADIUS_VERSION/rad-vscode-bicep.vsix --output /tmp/rad-vscode-bicep.vsix
