d05127c171fdaaeb548ccefabdd4be3c953cfe92
jasonviviano <83607984+jasonviviano@users.noreply.github.com>
2023-10-24 19:58:06 +0000

    Added a condition to allow Docker to fully setup during post-create before initializing the K3d cluster

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 ## Create a k3d cluster
-k3d cluster delete
-k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:0'
+while (! kubectl cluster-info ); do
+  # Docker takes a few seconds to initialize
+  echo "Waiting for Docker to launch..."
+  k3d cluster delete
+  k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:0'
+  sleep 1
+done
 
 ## Install Dapr and init
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
