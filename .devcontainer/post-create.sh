f8f0bd042827b252453ca60e61432971c9013b9c
Reshma Abdul Rahim <reshmarahim.abdul@microsoft.com>
2023-10-24 10:47:29 -0400

    Add root as remote user

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+## Create a k3d cluster
+k3d cluster delete
+k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:0'
+
 ## Install Dapr and init
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
 dapr uninstall # clean if needed
