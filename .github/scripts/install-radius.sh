80f83d0764f50fbb29ae285ca57464234dc83389
Will Smith <willdavsmith@gmail.com>
2023-11-06 12:06:10 -0800

    Add edge rad CLI pull to samples tests (#737)
    * Add install-radius script
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * chmod
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * test
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * test
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * test
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * test
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * test
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * test
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * test
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * PR
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * PR
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * PR
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * PR
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * PR
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    * PR
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    
    ---------
    
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>
    Signed-off-by: Will Tsai <28876888+willtsai@users.noreply.github.com>

---
+++ .github/scripts/install-radius.sh
@@ -0,0 +1,38 @@
+# ------------------------------------------------------------
+# Copyright 2023 The Radius Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#    
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------
+
+set -xe
+
+VERSION=$1
+RAD_CLI_URL=https://raw.githubusercontent.com/radius-project/radius/main/deploy/install.sh
+RAD_CLI_EDGE_URL=ghcr.io/radius-project/rad/linux-amd64:latest
+
+if [[ $VERSION == "edge" ]]; then
+    echo Downloading rad CLI edge version
+    oras pull $RAD_CLI_EDGE_URL
+    chmod +x ./rad
+    mv ./rad /usr/local/bin/rad
+elif [[ -n $VERSION && $VERSION != *"rc"* ]]; then
+    INPUT_CHANNEL=$(echo $VERSION | cut -d '.' -f 1,2)
+    echo "Downloading rad CLI version $INPUT_CHANNEL"
+    wget -q $RAD_CLI_URL -O - | /bin/bash -s $INPUT_CHANNEL
+elif [[ -n $VERSION ]]; then
+    echo Downloading rad CLI version $VERSION
+    wget -q $RAD_CLI_URL -O - | /bin/bash -s $VERSION
+else
+    echo Downloading latest rad CLI
+    wget -q $RAD_CLI_URL -O - | /bin/bash
+fi
