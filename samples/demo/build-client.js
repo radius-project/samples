80f40228704eda2785aeacb7c5c00c326d18847b
Ryan Nowak <nowakra@gmail.com>
2024-01-04 15:21:23 -0800

    Fix local build/run for demo container
    For some reason the copy step for frontend assets is in the Dockerfile instead of the build script. This meant that the local build didn't work. Moving this step into the build and removing it from the docker file should address the issue.
    
    Signed-off-by: Ryan Nowak <nowakra@gmail.com>

--- samples/demo/build-client.js
+++ samples/demo/build-client.js
@@ -1,2 +1,3 @@
 const opts = { stdio: 'inherit', cwd: 'client', shell: true };
 require('child_process').execSync('npm run build', opts);
+require('fs').cpSync('client/build', 'dist/www', { recursive: true });
