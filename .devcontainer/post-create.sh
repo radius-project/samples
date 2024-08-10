bd68356a9e53666f6b1a6534cb8c64a88d48aa2c
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2024-08-10 17:38:19 +0000

    Update k3d create commands
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,7 +5,7 @@ while (! kubectl cluster-info ); do
   # Docker takes a few seconds to initialize
   echo "Waiting for Docker to launch..."
   k3d cluster delete
-  k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:0'
+  k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:*' --k3s-arg '--disable=servicelb@server:*'
   sleep 1
 done
 
