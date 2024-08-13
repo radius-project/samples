bbb7130b8c6377297daa566b23f0083997a26204
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2024-08-13 18:03:15 +0000

    Add comment describing the k3d configuration
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,6 +5,7 @@ while (! kubectl cluster-info ); do
   # Docker takes a few seconds to initialize
   echo "Waiting for Docker to launch..."
   k3d cluster delete
+  # Map localhost port 80 on the external load balancer, and disable traefik and the internal load balancer.
   k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:*' --k3s-arg '--disable=servicelb@server:*'
   sleep 1
 done
