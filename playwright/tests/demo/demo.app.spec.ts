716362a42f350e681c1ebb3a64c5bef2e9e10389
Zach Casper <zachcasper@microsoft.com>
2026-08-14 17:38:03 -0500

    Removed redis checks for demo app
    Signed-off-by: Zach Casper <zachcasper@microsoft.com>

--- playwright/tests/demo/demo.app.spec.ts
+++ playwright/tests/demo/demo.app.spec.ts
@@ -38,25 +38,6 @@ test("To-Do App Basic UI Checks", async ({ page }) => {
 
   // Go back to Main Page
   await page.getByRole("link", { name: "Radius Demo" }).click();
-
-  await page.getByRole("button", { name: "📄 Environment variables" }).click();
-
-  // Make sure important environment variables are visible
-  await expect(
-    page
-      .getByRole("cell", { name: "CONNECTION_REDIS_CONNECTIONSTRING" })
-      .getByText("CONNECTION_REDIS_CONNECTIONSTRING")
-  ).toBeVisible();
-  await expect(
-    page
-      .getByRole("cell", { name: "CONNECTION_REDIS_HOST" })
-      .getByText("CONNECTION_REDIS_HOST")
-  ).toBeVisible();
-  await expect(
-    page
-      .getByRole("cell", { name: "CONNECTION_REDIS_PORT" })
-      .getByText("CONNECTION_REDIS_PORT")
-  ).toBeVisible();
 });
 
 test("Add an item and check basic functionality", async ({ page }) => {
